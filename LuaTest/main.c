c6ad219da16e8eff363a7eea6bc6230b1812c416
yuedaregone <810566249@qq.com>
2015-12-27 00:02:45 +0800

    asd

--- LuaTest/main.c
+++ LuaTest/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lua/lua.h"
+#include <lua/lua.h>
 #include <lua/lualib.h>
 #include <lua/lauxlib.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 static int my_add(lua_State* L)
 {
 	int a = luaL_checknumber(L, 1);
@@ -17,27 +17,33 @@ static const luaL_Reg MyLib[] = {
 	{NULL, NULL}
 };
 
-
-
+static void luaopen_mylib(lua_State* L)
+{
+	luaL_newlib(L, MyLib);
+    lua_setglobal(L, "MyLib");
+}
 
 int main(int argc, char *argv[]) {
 	lua_State* L = luaL_newstate();		
 	luaL_openlibs(L);
-	
-	//luaL_newlib(L, MyLib);
-	//luaL_requiref(L, , lib->func, 1);
-    //lua_pop(L, 1); 
-    //lua_register(L, "MyLib", MyLib);
-    luaL_newlib(L, MyLib);
-    lua_setglobal(L, "MyLib");
+    luaopen_mylib(L);
     
-    
-	
 	if (luaL_loadfile(L, "./main.lua") || lua_pcall(L, 0, 0, 0))
 	{
 		printf("LUA ERROR:%s", lua_tostring(L, -1));
 		lua_close(L);
 		return -1;
-	}	
+	}
+	
+	lua_getglobal(L, "my_lua_fun");
+	lua_pcall(L, 0, 0, 0);
+	
+	
+	lua_getglobal(L, "my_lua_add");
+	lua_pushnumber(L, 10);
+	lua_pushnumber(L, 20);
+	lua_pcall(L, 2, 1, 0);
+	int param = lua_tonumber(L, -1);
+	printf("the result is %d", param);
 	return 0;
 }
--- LuaTest/main.lua
+++ LuaTest/main.lua
@@ -1,3 +1,12 @@
 print("Hello World!")
 print(math.abs(-10))
 print(MyLib.my_add(1, 100))
+
+function my_lua_fun()
+	print("this is a lua function!");
+end
+
+function my_lua_add(param1, param2)	
+	print("param1:"..param1, "param2:"..param2);
+	return param1+param2;
+end
